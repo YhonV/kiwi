d31e21946d84327e2c3beebd396b439a2a08a024
yvivas <yhonvivas16@gmail.com>
2026-06-04 19:20:03 -0400

    Initial Commit

---
+++ kiwi/kiwiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiwiApp.swift
+//  kiwi
+//
+//  Created by Yhon Vivas on 04-06-26.
+//
+
+import SwiftUI
+
+@main
+struct kiwiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
